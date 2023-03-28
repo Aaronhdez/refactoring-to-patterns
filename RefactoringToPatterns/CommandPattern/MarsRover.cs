012fe406a57c5325ede800ab1cf0c223219ed207
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-28 11:33:22 +0100

    Refactor: commands extracted from execution methods

--- RefactoringToPatterns/CommandPattern/MarsRover.cs
+++ RefactoringToPatterns/CommandPattern/MarsRover.cs
@@ -2,26 +2,103 @@ using System.Linq;
 
 namespace RefactoringToPatterns.CommandPattern
 {
+    public class MoveWestCommand
+    {
+        private MarsRover _marsRover;
+
+        public MoveWestCommand(MarsRover marsRover)
+        {
+            _marsRover = marsRover;
+        }
+
+        public void Execute()
+        {
+            _marsRover.ObstacleFound = _marsRover.Obstacles.Contains($"{_marsRover.X - 1}:{_marsRover.Y}");
+            // check if rover reached plateau limit or found an obstacle
+            _marsRover.X = _marsRover.X > 0 && !_marsRover.ObstacleFound ? _marsRover.X -= 1 : _marsRover.X;
+        }
+    }
+
+    public class MoveNorthCommand
+    {
+        private MarsRover _marsRover;
+
+        public MoveNorthCommand(MarsRover marsRover)
+        {
+            _marsRover = marsRover;
+        }
+
+        public void Execute()
+        {
+            _marsRover.ObstacleFound = _marsRover.Obstacles.Contains($"{_marsRover.X}:{_marsRover.Y - 1}");
+            // check if rover reached plateau limit or found an obstacle
+            _marsRover.Y = _marsRover.Y > 0 && !_marsRover.ObstacleFound ? _marsRover.Y -= 1 : _marsRover.Y;
+        }
+    }
+
+    public class MoveSouthCommand
+    {
+        private MarsRover _marsRover;
+
+        public MoveSouthCommand(MarsRover marsRover)
+        {
+            _marsRover = marsRover;
+        }
+
+        public void Execute()
+        {
+            _marsRover.ObstacleFound = _marsRover.Obstacles.Contains($"{_marsRover.X}:{_marsRover.Y + 1}");
+            // check if rover reached plateau limit or found an obstacle
+            _marsRover.Y = _marsRover.Y < 9 && !_marsRover.ObstacleFound ? _marsRover.Y += 1 : _marsRover.Y;
+        }
+    }
+
+    public class MoveEastCommand
+    {
+        private MarsRover _marsRover;
+
+        public MoveEastCommand(MarsRover marsRover)
+        {
+            _marsRover = marsRover;
+        }
+
+        public void Execute()
+        {
+            _marsRover.ObstacleFound = _marsRover.Obstacles.Contains($"{_marsRover.X + 1}:{_marsRover.Y}");
+            // check if rover reached plateau limit or found an obstacle
+            _marsRover.X = _marsRover.X < 9 && !_marsRover.ObstacleFound ? _marsRover.X += 1 : _marsRover.X;
+            return;
+        }
+    }
+
     public class MarsRover
     {
-        private int _x;
-        private int _y;
+        public int X;
+        public int Y;
         private char _direction;
         private readonly string _availableDirections = "NESW";
-        private readonly string[] _obstacles;
-        private bool _obstacleFound;
+        public readonly string[] Obstacles;
+        public bool ObstacleFound;
+        private readonly MoveWestCommand _moveWestCommand;
+        private readonly MoveNorthCommand _moveNorthCommand;
+        private readonly MoveSouthCommand _moveSouthCommand;
+        private readonly MoveEastCommand _moveEastCommand;
 
         public MarsRover(int x, int y, char direction, string[] obstacles)
         {
-            _x = x;
-            _y = y;
+            X = x;
+            Y = y;
             _direction = direction;
-            _obstacles = obstacles;
+            Obstacles = obstacles;
+            _moveWestCommand = new MoveWestCommand(this);
+            _moveNorthCommand = new MoveNorthCommand(this);
+            _moveSouthCommand = new MoveSouthCommand(this);
+            _moveEastCommand = new MoveEastCommand(this);
         }
         
         public string GetState()
         {
-            return !_obstacleFound ? $"{_x}:{_y}:{_direction}" : $"O:{_x}:{_y}:{_direction}";
+            return !ObstacleFound ? $"{X}:{Y}:{_direction}" : $"O:{X}:{Y}:{_direction}";
         }
 
         public void Execute(string commands)
@@ -33,16 +110,16 @@ namespace RefactoringToPatterns.CommandPattern
                     switch (_direction)
                     {
                         case 'E':
-                            MoveEast();
+                            _moveEastCommand.Execute();
                             break;
                         case 'S':
-                            MoveSouth();
+                            _moveSouthCommand.Execute();
                             break;
                         case 'W':
-                            MoveWest();
+                            _moveWestCommand.Execute();
                             break;
                         case 'N':
-                            MoveNorth();
+                            _moveNorthCommand.Execute();
                             break;
                     }
                 }
@@ -82,35 +159,6 @@ namespace RefactoringToPatterns.CommandPattern
             {
                 _direction = _availableDirections[3];
             }
-        }
-
-        private void MoveNorth()
-        {
-            _obstacleFound = _obstacles.Contains($"{_x}:{_y - 1}");
-            // check if rover reached plateau limit or found an obstacle
-            _y = _y > 0 && !_obstacleFound ? _y -= 1 : _y;
-        }
-
-        private void MoveWest()
-        {
-            _obstacleFound = _obstacles.Contains($"{_x - 1}:{_y}");
-            // check if rover reached plateau limit or found an obstacle
-            _x = _x > 0 && !_obstacleFound ? _x -= 1 : _x;
-        }
-
-        private void MoveSouth()
-        {
-            _obstacleFound = _obstacles.Contains($"{_x}:{_y + 1}");
-            // check if rover reached plateau limit or found an obstacle
-            _y = _y < 9 && !_obstacleFound ? _y += 1 : _y;
-        }
-
-        private void MoveEast()
-        {
-            _obstacleFound = _obstacles.Contains($"{_x + 1}:{_y}");
-            // check if rover reached plateau limit or found an obstacle
-            _x = _x < 9 && !_obstacleFound ? _x += 1 : _x;
-            return;
         }
     }
 }
